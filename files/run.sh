81100aab12f33827f4ea679a63d7d5cee2183ea0
nurikabe <nurikabe@yahoo.com>
2018-11-07 14:44:53 -0500

    Use our env variable $VIRTUAL_HOST to hard-code the domain name

--- files/run.sh
+++ files/run.sh
@@ -33,7 +33,7 @@ if [ "$VIRTUAL_HOST" ] && [ $LETSENCRYPT_EMAIL ] && [ ! -s ./certs.conf ] ; then
   printf "ssl_certificate_key /etc/letsencrypt/live/$VIRTUAL_HOST/privkey.pem;\n" > /etc/nginx/certs.conf
   printf "ssl_certificate /etc/letsencrypt/live/$VIRTUAL_HOST/fullchain.pem;\n" >> /etc/nginx/certs.conf
   # redirect http traffic to https
-  printf 'server { listen 80; return 301 https://$server_name$request_uri; }' > /etc/nginx/redirect.conf
+  printf "server { listen 80; return 301 https://$VIRTUAL_HOST\$request_uri; }" > /etc/nginx/redirect.conf
   # start crond in the background
   crond
 fi
